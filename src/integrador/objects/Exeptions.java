c5bd252a2a8270684e919c16dc6a37e1fb7c4c8b
RenzoAhumada <94655050+RenzoAhumada@users.noreply.github.com>
2023-10-29 23:52:00 -0300

    se modificaron datos exception y prestamo

--- src/integrador/objects/Exeptions.java
+++ src/integrador/objects/Exeptions.java
@@ -2,6 +2,9 @@
 package integrador.objects;
 
 
-public class Exeptions {
-    
+public class Exeptions extends Exception{
+    public void exceptionForPrestamo(){
+        System.out.println("No se puede prestar el libro ya que no hay");
+        
+}
 }
--- src/integrador/objects/Prestamo.java
+++ src/integrador/objects/Prestamo.java
@@ -1,9 +1,38 @@
 
 package integrador.objects;
 
+import java.util.Scanner;
+
 
 public class Prestamo extends Libro {
-    private boolean pass;
+    private boolean pass=true;
+
+    public Prestamo() {
+    }
+
+    public boolean isPass() {
+        return pass;
+    }
+
+    public void setPass(boolean pass) {
+        this.pass = pass;
+    }
+
+    public Scanner getSc() {
+        return sc;
+    }
+
+    public void setSc(Scanner sc) {
+        this.sc = sc;
+    }
     
-    
+    public void bookPass(){
+        Libro book = new Libro();
+        Exeptions e = new Exeptions();
+        if(book.getCuantityBooks()==0){
+            e.exceptionForPrestamo();
+        }else{
+            book.setCuantityBooks(book.getCuantityBooks()-1);           
+        }
+}
 }
